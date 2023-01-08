1d5de372a33635c89f46b3048219e22fc9d055ca
Amirkhan Kuncheraev <akuncheraev@gmail.com>
2023-01-09 02:49:27 +0300

    add sticky nav

--- js/script.js
+++ js/script.js
@@ -38,3 +38,30 @@ btnNavEl.addEventListener("click", function () {
   // navItemEl.classList.toggle("nav-open");
   navItemEl.forEach((item) => item.classList.toggle("open"));
 });
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
